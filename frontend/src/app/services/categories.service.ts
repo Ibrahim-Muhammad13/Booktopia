90f22cd3f1af93b3c5cec88847d4417aa3879a9b
Nournate <noranateee@gmail.com>
2023-05-24 17:18:18 +0300

    in categories add form for edit option with two buttons update and cancel

--- frontend/src/app/services/categories.service.ts
+++ frontend/src/app/services/categories.service.ts
@@ -1,32 +1,31 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Category } from '../models/category';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-getCategories(){
-  return this.http.get('http://localhost:3000/admin/category')
+  getCategories(): Observable<any> {
+    return this.http.get('http://localhost:3000/admin/category');
+  }
+
+  createCategory(cat: string): Observable<any> {
+    const category = { cat_Name: cat };
+    return this.http.post('http://localhost:3000/admin/category', category);
+  }
+
+  deleteCategory(catId: number): Observable<any> {
+    return this.http.delete('http://localhost:3000/admin/category/' + catId);
+  }
+
+  updateCategory(catId: number, cat: string): Observable<any> {
+    const category = { cat_Name: cat };
+    return this.http.put(`http://localhost:3000/admin/category/` + catId, category);
+  }
 }
-
-createCategory(cat:string){
-  const category = {cat_Name:cat}
-  return this.http.post('http://localhost:3000/admin/category',category).subscribe((res:any)=>console.log(res))
-}
-
-deleteCategory(catId: number) {
-  return this.http.delete('http://localhost:3000/admin/category/'+catId).subscribe();
-}
-
-updateCategory(catId: number, cat: string) {
-  const category = { cat_Name: cat };
-  return this.http.put(`http://localhost:3000/admin/category/`+catId, category).subscribe((res:any)=>console.log(res));
-}
-
-
-}
-
