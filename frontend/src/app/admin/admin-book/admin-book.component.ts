905654f8ace9560b577905ef1b1ca3a24decccf7
Nournate <noranateee@gmail.com>
2023-05-26 23:32:23 +0300

    update in  admin/book

--- frontend/src/app/admin/admin-book/admin-book.component.ts
+++ frontend/src/app/admin/admin-book/admin-book.component.ts
@@ -6,8 +6,9 @@ import { AutherService } from 'src/app/services/auther.service';
 import { BookService } from 'src/app/services/book.service';
 import { Category } from 'src/app/models/category';
 import { CategoriesService } from 'src/app/services/categories.service';
-import { Subscription } from 'rxjs';
+import { Subscription ,Observer } from 'rxjs';
 import { FormGroup } from '@angular/forms';
+
 
 @Component({
   selector: 'app-admin-book',
@@ -40,85 +41,32 @@ constructor(private http:HttpClient, private auther:AutherService, private book:
  
   
   }
+  saveBook() {
+    const newBook: Book = {
+      _id: 0,
+      name: this.bookName,
+      rate: this.rate,
+      authorId: this.autherId,
+      categoryId: this.categoryId,
+      image: ''
+    };
+    this.books.push(newBook);
+    this.cancelForm();
+    this.book.addBook(this.bookName, this.autherId, this.categoryId, this.rate)
+  }
+
   getauthers(){
     this.auther.getAllauther().subscribe((res:any)=>this.authers=res);
   }
+
   getbooks(){
   this.book.getBooks().subscribe((res:any)=>this.books=res);
   }
+
   getcategories(){
   this.category.getCategories().subscribe((res:any)=>this.categories=res.categories);
   }
   
-
-  items!: Book[];
-  newItem!: Book
-  addItem() {
-    this.items.push(this.newItem);
-    this.showForm = false;
-  }
-  // addBook(){
-  //   this.book.addBook(this.bookName,this.autherId,this.categoryId,this.rate)
-  //   this.getbooks();
-  //   this.bookName = '';
-  //   this.rate = 0;
-  //   this.autherId = '';
-  //   this.categoryId = 0;
-  //   this.cancelForm();
-  // }
-  // addBook() {
-  //   this.book.addBook(this.bookName, this.autherId, this.categoryId, this.rate)
-  //     .subscribe((res: any) => {
-  //       console.log('Book added successfully');
-  //       const newBook: Book = {
-  //         _id: res.book._id,
-  //         name: this.bookName,
-  //         rate: this.rate,
-  //         authorId: this.autherId,
-  //         categoryId: this.categoryId,
-  //         image: '',
-  //       };
-  //       this.books.push(newBook); // add the new book to the array
-  //       this.bookName = ''; // reset the form fields
-  //       this.rate = 0;
-  //       this.autherId = '';
-  //       this.categoryId = 0;
-  //       this.showForm = false; // show the table again
-  //       this.fetchBooks();
-  //       this.cancelForm();
-  //     }, (err: any) => {
-  //       console.error(err); // handle error
-  //     });
-  // }
-
-  addBook() {
-    console.log('Book name:', this.bookName);
-    console.log('Author ID:', this.autherId);
-    console.log('Category ID:', this.categoryId);
-    console.log('Rate:', this.rate);
-    this.book.addBook(this.bookName, this.autherId, this.categoryId, this.rate)  
-  }
-
-  // addBook(name:string,autherId:string,categoryId:number,rate:number){
-  //   const newBook: Book = {
-  //     _id:0,
-  //     name: this.bookForm.controls['bookName']?.value,
-  //     rate: this.bookForm.controls['rate']?.value,
-  //     authorId: this.bookForm.controls['autherId']?.value,
-  //     categoryId:this.bookForm.controls['categoryId']?.value,
-  //     image: this.bookForm.controls['image']?.value,
-  //   };
-  //   this.book.addBook(name,autherId,categoryId,rate);
-  // //   .subscribe((res: any) => {
-  // //   this.fetchBooks();
-  // //   this.cancelForm();
-  // // });
-  //   this.books.push(newBook);  //push to ui whithout refresh
-  //   this.bookForm.controls['categoryName'].setValue(''); //empty the input field
-  //   this.showForm = false; // to back again to table when save button clicked
-  // }
-
-
   editBook(book: Book) {
     this.selectedBook = book;
     this.newBookName = book.name;
@@ -126,35 +74,47 @@ constructor(private http:HttpClient, private auther:AutherService, private book:
     this.isNewBook = false;
   }
 
-  updateBook(bookId: number, book: string) {
-    const newBookName = this.bookForm.controls['bookName'].value;
-    console.log(newBookName);
-    this.book.updateBook(bookId, newBookName)
-  }
-
-
-
-  deleteBookFromTabel(index: number) {
-    const bookId = this.books[index]._id;
-    this.books.splice(index, 1); // delete from table ui
-    this.book.deleteBook(bookId).subscribe((res: any) => {
-    this.getbooks();
+  
+  updateBook(bookId: number, bookName: string) {
+    const updatedBook: Book = {
+      _id: bookId,
+      name: bookName,
+      rate: this.rate,
+      authorId: this.autherId,
+      categoryId: this.categoryId,
+      image: ''
+    };
+  
+    this.book.updateBook(bookId, bookName).subscribe({
+      next: (res: any) => {
+        console.log('Book updated successfully:', res);
+        // Find the index of the updated book in the array
+        const index = this.books.findIndex((book) => book._id === bookId);
+        if (index !== -1) {
+          // Update the book in the array
+          this.books[index] = updatedBook;
+        }
+        this.cancelForm();
+      },
+      error: (err: any) => {
+        console.error(err);
+      }
     });
   }
-
+  
+  
+  
+  
 
   deleteBook(bookId: number, index: number) {
-    this.deleteBookFromTabel(index);
-    console.log(bookId);
-    console.log('Book deleted successfully');
-    this.category.deleteCategory(bookId).subscribe((res: any) => {
-      this.getbooks();
-    },(err: any) => {
-          console.error(err);
-    });
+    this.deleteBookFromTable(index);
+    this.book.deleteBook(bookId)
   }
-
-
+  
+  deleteBookFromTable(index: number) {
+    this.books.splice(index, 1); 
+  }
+  
   cancelForm() {
     this.selectedBook = null;
     this.newBookName = '';
@@ -162,12 +122,10 @@ constructor(private http:HttpClient, private auther:AutherService, private book:
     this.isNewBook = false;
   }
 
-
   ngOnDestroy() {
     if (this.subscription) {
     this.subscription.unsubscribe();
     }
     }
 }
-
 
