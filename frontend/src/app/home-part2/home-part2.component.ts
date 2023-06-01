7009548bbe12d61c9662a4bd5f592d0d830c347c
Nournate <noranateee@gmail.com>
2023-06-01 15:08:44 +0300

    still

--- frontend/src/app/home-part2/home-part2.component.ts
+++ frontend/src/app/home-part2/home-part2.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-part2.component.css']
 })
 export class HomePart2Component {
-
+  image1: string ="../../assets/images/cover44.jpeg";
+  image2: string ="../../assets/images/cover2.jpeg";
+  image3: string ="../../assets/images/cover3.jpeg";
+  image4: string ="../../assets/images/book_bundles.png";
 }
