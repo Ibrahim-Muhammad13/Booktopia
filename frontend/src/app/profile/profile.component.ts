978cb154fc85afcbd0c08dab00939127e628f774
Nournate <noranateee@gmail.com>
2023-06-03 02:18:03 +0300

    update alert for the auther books and profile

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -16,7 +16,10 @@ export class ProfileComponent {
   books:any;
   id_user!:any
   Auther!:[{}]
-
+  selectedOption: string = "";
+  showAlert: boolean = false;
+  alertMessage: string = "";
+  index!:any
 
   ngOnInit(){
     this.id_user=this.auth.getTokenID()
@@ -29,6 +32,11 @@ export class ProfileComponent {
     });
 
   }
+
+ 
+
+ 
+
 
 
 
@@ -54,19 +62,50 @@ export class ProfileComponent {
   wanttoread(){this.user_book.getbooks(this.id_user,"wanttoread").subscribe((res:any)=>{
     this.books=res;
     });}
-
-changestates(index:any,newStutes:string){
-const newdata={
-  "bookid":this.books[index].bookid._id,
-   "status":newStutes,
-   "UserId":this.books[index].UserId,
-   "rate":this.books[index].bookid.rate
-}
-this.user_book.update(this.books[index]._id,newdata).subscribe((res:any)=>{
-  console.log(res);
-  })
-  
+    
+    showAlertMessage() {
+      if (this.auth.isAuth()) {
+        if (this.selectedOption) {
+          const selectedOptionName = this.getOptionName(this.selectedOption);
+          this.alertMessage = "Option selected: " + selectedOptionName;
+        } else {
+          this.alertMessage = "Please select an option.";
+        }
+        this.showAlert = true;
+      } else {
+        this.alertMessage = "Please log in before selecting an option.";
+        this.showAlert = true;
+      }
     }
-
+  getOptionName(optionValue: string): string {
+    if (optionValue === "read") {
+        return "read";
+    } else if (optionValue === "currently") {
+        return "currently";
+    } else if (optionValue === "wanttoread") {
+        return "want to read";
+    } else {
+        return ""; 
+    }
+  }
+  
+    changestates(index: any, newStatus: string) {
+      const newData = {
+        "bookid":this.books[index].bookid._id,
+           "status":newStatus,
+           "UserId":this.books[index].UserId,
+           "rate":this.books[index].bookid.rate
+      };
+    
+      this.user_book.update(this.books[index]._id, newData).subscribe((res: any) => {
+        console.log(res);
+      });
+    
+    }
+     
+hideAlert() {
+  this.showAlert = false;
+  this.alertMessage = "";
+}
 }
 
