8cd1627334ebda04bc424b68731386f31e8c8553
Nournate <noranateee@gmail.com>
2023-06-03 23:39:47 +0300

    add book design & register message

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -80,7 +80,7 @@ export class AuthService {
 
   }
   register(data:any){
-    return this.http.post('http://localhost:3000/auth/register',data).subscribe((res:any)=>console.log(res))
+    return this.http.post('http://localhost:3000/auth/register',data)
   }
   login(data:any){
     // console.log(data)
